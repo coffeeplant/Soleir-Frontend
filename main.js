5105851c57121db12cdb7adff2b087b8127982b1
Bebhinn Hare <Bebhin@Duncans-iMac.local>
2021-04-19 22:12:25 +0100

    Signin working with authentication
    username and password hardcoded

--- main.js
+++ main.js
@@ -122,7 +122,7 @@ if (document.getElementById('appointment')) {
 
         },
         deleteNote () {
-          //the empty note should be updated to the database
+          //the empty note should be updated to the database by running the mutation again
           console.log('deleteNote');
           this.editing = false;
         }
@@ -130,7 +130,7 @@ if (document.getElementById('appointment')) {
       mounted () {
         console.log('SoleirAPI, ', SoleirAPI);
         //extracts apptid from the url, userid is not passed into the url, considering how
-        //toekns can be used correctly here
+        //tokens can be used correctly here
         this.apptID = window.location.hash.substr(1);
         axios({
           // url: 'http://app-soleir-spring-graphql.azurewebsites.net/graphql',
@@ -169,6 +169,77 @@ if (document.getElementById('appointment')) {
 
 
 
+
+
+
+
+
+
+
+
+if (document.getElementById('home')) {
+
+
+
+      var app = new Vue({
+        el: '#home',
+      data () {
+          return {
+              login: {
+                email: null,
+                password: null
+              }
+          }
+        },
+      mounted () {
+      },
+      methods: {
+        async loginUser() {
+          console.log('you did the thing ' + this.login.email);
+          console.log(SoleirAPI);
+          axios({
+            // url: 'http://app-soleir-spring-graphql.azurewebsites.net/graphql',
+            url: SoleirAPI,
+            method: 'post',
+            data: {
+              query: `mutation 
+                { 
+                  signinUser(input:{email:"test@userdb.com", soleirID:"UDBID001"}) {
+                     token  
+                     userID
+                  }
+                }
+                `
+            }
+          }).then((result) => {
+            if (!result.data.data.signinUser) {
+              alert('ERROR');
+              return;
+            }
+            console.log('result, ', result);
+            console.log('userID, ', result.data.data.signinUser.userID);
+            localStorage.setItem("user", result.data.data.signinUser.userID);
+            localStorage.setItem("token", result.data.data.signinUser.token);
+
+            console.log('localStorage' , localStorage.getItem('user'));
+            console.log('localStorage token' , localStorage.getItem('token'));
+            // this.appointment = result.data.data;
+            window.location.replace("/appointments.html");
+          })
+          // try {
+          //   let response = await this.$http.post("/auth/login", this.login);
+          //   let token = response.data.data.token;
+          //   localStorage.setItem("user", token);
+          //   // navigate to a protected resource 
+          //   this.$router.push("/me");
+          // } catch (err) {
+          //   console.log(err.response);
+          // }
+        }
+      }
+    });
+
+}//end of if appointment
 
 
 
